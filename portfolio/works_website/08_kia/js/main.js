31c95045d8595cc7ede5f82328a4d5842c1bfdb4
Kim Yeong jin <inusneo@gmail.com>
2021-10-25 15:04:03 +0900

    First complete CSS & js

--- portfolio/works_website/08_kia/js/main.js
+++ portfolio/works_website/08_kia/js/main.js
@@ -3,7 +3,6 @@
 /* Gnb Scroll Up & Down Effects */
 
 const header = document.querySelector('#header');
-const sections = document.querySelectorAll('section');
 const gnbMenu = document.querySelectorAll('.gnb a');
 let lastScroll = 0;
 
@@ -11,86 +10,43 @@ window.addEventListener('scroll', () => {
   const currentScroll = window.pageYOffset;
   //console.log('scroll', scroll);
   if (currentScroll <= 0) {
-    header.classList.remove('scroll-up');
+    header.classList.remove('on');
   } 
-  if (currentScroll > lastScroll && !header.classList.contains('scroll-down')) { 
-    header.classList.remove('scroll-up');
-    header.classList.add('scroll-down');
+  if (currentScroll > lastScroll) { 
+    header.classList.add('on');
   }  
-  if (currentScroll < lastScroll && header.classList.contains('scroll-down')) { 
-    header.classList.remove('scroll-down');
-    header.classList.add('scroll-up');
-  } 
-
-  lastScroll = currentScroll;
-
-  let cur = '';
-
-  sections.forEach( section => {
-    const sectionTop = section.offsetTop;
-    const sectionHt = section.clientHeight;
-
-    if (pageYOffset >= sectionTop) {
-      cur = section.getAttribute('id');
-    }
-  });
-
-  gnbMenu.forEach( list => {
-    list.classList.remove('active');
-
-    if (list.classList.contains(cur)) {
-      list.classList.add('active');
-    }
-  });
 });
 
 /* Hamburger Menu */
 
-const hamburg = document.querySelector('.btn-menu');
-const dim = document.querySelector('.dim');
+const btnOpen = document.querySelector('.btn--open');
+const btnClose = document.querySelector('.btn--close');
 const gnbDrawer = document.querySelector('#gnb');
 
-hamburg.addEventListener('click', (e) => {
-  e.currentTarget.classList.toggle('on');
-  gnbDrawer.classList.toggle('active');
-  mBody.classList.toggle('on');
-  dim.classList.toggle('on');
+btnOpen.addEventListener('click', (e) => {
+  gnbDrawer.classList.add('on');
+});
+btnClose.addEventListener('click', (e) => {
+  gnbDrawer.classList.remove('on');
 });
 
-/* Drawer Menu Click Function */
+/* Site all Menu */
 
-const drawerMenu = document.querySelectorAll('.gnb__wrap a');
+const btnSite = document.querySelectorAll('.site-all__menu');
 
-drawerMenu.forEach(menu => {
-  menu.addEventListener('click', () => {
-    hamburg.classList.remove('on');
-    gnbDrawer.classList.remove('active');
-    mBody.classList.remove('on');
-    dim.classList.remove('on');
-  });
+btnSite.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    item.classList.toggle('on');
+  })
 });
 
-/* 🖱️ Scroll Top Btn display */
+/* Family Site Menu  */
 
-const btnScrollBox = document.querySelector('.btn-top');
+const btnFamily = document.querySelector('.footer__family--btn');
+const familyList = document.querySelector('.footer__family');
 
-window.addEventListener('scroll', () => {
-  if (document.querySelector('html').scrollTop > 150) {
-    btnScrollBox.style.display = 'block';
-  } else {
-    btnScrollBox.style.display = 'none';
-  }
-});
-
-/* 🖱️ Scroll Top effect */
-
-const btnScrollTop = document.querySelector('#btnScrTop');
-
-btnScrollTop.addEventListener('click', (e) => {
+btnFamily.addEventListener('click', (e) => {
   e.preventDefault();
-  window.scroll({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
+  familyList.classList.toggle('on');
 });
