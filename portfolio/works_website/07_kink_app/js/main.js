0a9618fc964257fbc5ad3713eecb7e83e691f995
Kim Yeong jin <inusneo@gmail.com>
2021-11-04 18:21:47 +0900

    main index btn-circle display change, kink app add drawer

--- portfolio/works_website/07_kink_app/js/main.js
+++ portfolio/works_website/07_kink_app/js/main.js
@@ -4,4 +4,8 @@ $(document).ready(function () {
   $('.tab-bar ul > li').eq(4).click(function () {
     alert('Comming Soon...');
   });
+
+  $('.btn-drawer').click(function () {
+    $('.drawer').toggleClass('on');
+  });
 });
