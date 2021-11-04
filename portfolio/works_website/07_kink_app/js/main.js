2d669f18662e5042e886b74bc73cf121ae085e3a
Kim Yeong jin <inusneo@gmail.com>
2021-11-04 23:32:49 +0900

    add my page

--- portfolio/works_website/07_kink_app/js/main.js
+++ portfolio/works_website/07_kink_app/js/main.js
@@ -1,10 +1,6 @@
 /* main.js */
 
 $(document).ready(function () {
-  $('.tab-bar ul > li').eq(4).click(function () {
-    alert('Comming Soon...');
-  });
-
   $('.btn-drawer').click(function () {
     $('.drawer').toggleClass('on');
   });
